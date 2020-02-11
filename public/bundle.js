75ebd4ba65787789d5cc0b393e91b95f6133bedc
hontas <lundin.pontus@gmail.com>
2020-02-11 23:26:48 +0100

    mkae border-box work n add a bunch of stuffs for dialog

--- public/bundle.js
+++ public/bundle.js
@@ -119,10 +119,12 @@ class IntObserver extends HTMLElement {
 
 customElements.define('intersection-observer', IntObserver);
 
-var styles$1 = ":host{display:block;position:fixed;top:0;left:0;width:100vw;height:100vh;pointer-events:none}.Dialog__overlay{top:0;left:0;height:100%;transition:opacity .3s;transition-delay:.15s;background-color:rgba(0,0,0,.5)}.Dialog__content,.Dialog__overlay{position:absolute;width:100%;opacity:0;pointer-events:none}.Dialog__content{bottom:0;transition:opacity .3s,transform .3s;transform:translateY(var(--Dialog-offset-y,0))}.Dialog--open .Dialog__content,.Dialog--open .Dialog__overlay{opacity:1;pointer-events:auto}.Dialog--open .Dialog__content{transition-delay:.15s}.Dialog__drag-handle{color:hsla(0,0%,100%,.75);cursor:grab;position:absolute;top:-1.5rem;left:0;height:1.5rem;width:100%;text-align:center;user-select:none;display:flex;align-items:center;justify-content:center}.Dialog__drag-icon{display:inline-block;width:16px;height:8px}.Dialog__drag-icon,.Dialog__drag-icon:before{background-image:radial-gradient(currentColor 40%,transparent 0);background-size:4px 4px;background-position:0 100%;background-repeat:repeat-x}.Dialog__drag-icon:before{content:\"\";display:block;width:100%;height:33%}.Dialog--dragging .Dialog__drag-handle{cursor:grabbing}.Dialog--dragging .Dialog__content{transition:none}.Dialog--inactive .Dialog__overlay{opacity:0;pointer-events:none}.Dialog--inactive .Dialog__content{box-shadow:0 -.5em 1em rgba(0,0,0,.2)}.Dialog--inactive .Dialog__drag-handle{color:rgba(0,0,0,.8)}.Dialog--no-resize .Dialog__drag-handle{display:none}";
+var styles$1 = ".Dialog{position:fixed;top:0;left:0;width:100vw;height:100vh;pointer-events:none}.Dialog__overlay{top:0;left:0;height:100%;transition:opacity .3s;transition-delay:.15s;background-color:rgba(0,0,0,.5)}.Dialog__content,.Dialog__overlay{position:absolute;width:100%;opacity:0;pointer-events:none}.Dialog__content{bottom:0;transition:opacity .3s,transform .3s ease-out;transform:translateY(var(--Dialog-offset-y,0))}.Dialog--open .Dialog__content,.Dialog--open .Dialog__overlay{opacity:1;pointer-events:auto}.Dialog--open .Dialog__content{transition-delay:.15s}.Dialog__drag-handle{color:hsla(0,0%,100%,.75);cursor:grab;position:absolute;top:-1.5rem;left:0;height:1.5rem;width:100%;text-align:center;user-select:none;display:flex;align-items:center;justify-content:center}.Dialog__drag-icon{display:inline-block;width:16px;height:8px}.Dialog__drag-icon,.Dialog__drag-icon:before{background-image:radial-gradient(currentColor 40%,transparent 0);background-size:4px 4px;background-position:0 100%;background-repeat:repeat-x}.Dialog__drag-icon:before{content:\"\";display:block;width:100%;height:33%}.Dialog--dragging .Dialog__drag-handle{cursor:grabbing}.Dialog--dragging .Dialog__content{transition:none}.Dialog--inactive .Dialog__overlay{opacity:0;pointer-events:none}.Dialog--inactive .Dialog__content{box-shadow:0 -.5em 1em rgba(0,0,0,.2)}.Dialog--inactive .Dialog__drag-handle{color:rgba(0,0,0,.8)}.Dialog--no-resize .Dialog__drag-handle{display:none}";
 
 const clamp = ({ min, value, max }) => Math.min(Math.max(value, min), max);
 const getPageY = (evt) => (evt.type.startsWith('touch') ? evt.touches[0].pageY : evt.pageY);
+const isElement = ({ nodeType }) => nodeType === 1;
+const toBool = (value) => (value != null ? true : false);
 
 const template$1 = () => `
 <style>${styles$1}</style>
@@ -130,6 +132,7 @@ const template$1 = () => `
   <div class="Dialog__overlay" role="button"></div>
   <div class="Dialog__content">
     <div class="Dialog__drag-handle"><span class="Dialog__drag-icon"></span></div>
+    <slot name="header"></slot>
     <slot></slot>
   </div>
 </div>
@@ -137,7 +140,7 @@ const template$1 = () => `
 
 class Dialog extends HTMLElement {
   static get observedAttributes() {
-    return ['open', 'min-content-height', 'no-resize'];
+    return ['open', 'no-resize', 'minimize', 'snap-to-top'];
   }
 
   constructor() {
@@ -146,41 +149,80 @@ class Dialog extends HTMLElement {
     const shadow = this.attachShadow({ mode: 'open' });
     shadow.innerHTML = template$1();
 
+    /**
+     * elements
+     */
+    this.dialogEl = this.shadowRoot.querySelector('.Dialog');
+    this.overlayEl = this.shadowRoot.querySelector('.Dialog__overlay');
+    this.contentWrapperEl = this.shadowRoot.querySelector('.Dialog__content');
+    this.contentEl = this.shadowRoot
+      .querySelector('slot:not([name])')
+      .assignedNodes()
+      .find(isElement);
+    this.headerEl = this.shadowRoot
+      .querySelector('slot[name="header"]')
+      .assignedNodes()
+      .find(isElement);
+    this.dragEl = this.shadowRoot.querySelector('.Dialog__drag-handle');
+
+    /**
+     * props
+     */
     this.isOpen = false;
     this.connected = false;
     this.noResize = false;
+    this.canMinimize = false;
     this.minimized = false;
-    this.contentHeight = 0;
-    this.minContentHeight = 34; // height of header
+    this.snapToTop = false;
+    this.snapToBottomThreshold = 0;
+    this.contentHeight = this.contentWrapperEl.offsetHeight;
+    this.headerHeight = this.headerEl ? this.headerEl.offsetHeight : 0;
     this.isTouchDevice = navigator.maxTouchPoints > 0;
 
+    /**
+     * Bound methods
+     */
     this.close = this.close.bind(this);
     this.handleTouchStart = this.handleTouchStart.bind(this);
     this.handleTouchMove = this.handleTouchMove.bind(this);
     this.handleTouchEnd = this.handleTouchEnd.bind(this);
     this.handleOverlayClick = this.handleOverlayClick.bind(this);
-
-    this.dialogEl = this.shadowRoot.querySelector('.Dialog');
-    this.overlayEl = this.shadowRoot.querySelector('.Dialog__overlay');
-    this.contentEl = this.shadowRoot.querySelector('.Dialog__content');
-    this.dragEl = this.shadowRoot.querySelector('.Dialog__drag-handle');
   }
 
   attributeChangedCallback(name, oldVal, newValue) {
     switch (name) {
       case 'open': {
-        this.isOpen = newValue != null ? true : false;
-        if (this.isOpen) return this.open();
-        return this.close();
+        this.isOpen = toBool(newValue);
+        if (this.isOpen) {
+          this.open();
+        } else if (this.canMinimize) {
+          this.setAttribute('open', '');
+          this.minimize();
+        } else {
+          this.close();
+        }
+        break;
       }
-      case 'min-content-height': {
-        this.minContentHeight = Number(newValue);
-        return;
+      case 'snap-to-top': {
+        this.snapToTop = toBool(newValue);
+        if (this.snapToTop) {
+          const height = window.innerHeight - this.headerHeight - this.dragEl.offsetHeight;
+          this.contentEl.style.setProperty('height', height + 'px');
+          this.contentHeight = this.contentWrapperEl.offsetHeight;
+        } else {
+          this.contentEl.style.removeProperty('height');
+        }
+        break;
       }
       case 'no-resize': {
-        this.noResize = newValue != null ? true : false;
+        this.noResize = toBool(newValue);
         this.dialogEl.classList[this.noResize ? 'add' : 'remove']('Dialog--no-resize');
-        return;
+        break;
+      }
+      case 'minimize': {
+        this.canMinimize = toBool(newValue);
+        this.snapToBottomThreshold = window.innerHeight * 0.3;
+        break;
       }
       default: {
         console.log('unhandled attr change:', name);
@@ -190,23 +232,26 @@ class Dialog extends HTMLElement {
 
   connectedCallback() {
     this.connected = true;
-    this.contentHeight = this.contentEl.offsetHeight;
     this.registerEventListeners();
     this.setStyles({ offsetY: this.contentHeight });
+    if (this.canMinimize && !this.isOpen) {
+      this.setAttribute('open', '');
+      this.minimize();
+    }
   }
 
   disconnectedCallback() {
     this.removeEventListeners();
+    document.body.style.removeProperty('overflow');
   }
 
   handleOverlayClick(evt) {
     if (evt.target !== this.overlayEl) return;
-    this.isOpen = false;
-    this.removeAttribute('open');
-    this.dialogEl.classList.remove('Dialog--open');
+    this.close();
   }
 
   open(retries = 5) {
+    // because attribute change fire before connected-hook
     if (!this.connected && retries) return requestAnimationFrame(() => this.open(--retries));
     this.minimized = false;
     document.body.style.setProperty('overflow', 'hidden');
@@ -219,15 +264,19 @@ class Dialog extends HTMLElement {
     this.minimized = true;
     document.body.style.setProperty('overflow', 'auto');
     this.dialogEl.classList.add('Dialog--inactive');
-    this.setStyles({ offsetY: this.contentHeight - this.minContentHeight });
+    this.setStyles({ offsetY: this.contentHeight - this.headerHeight });
   }
 
   close() {
-    this.minimized = false;
-    document.body.style.setProperty('overflow', 'auto');
-    this.dialogEl.classList.remove('Dialog--open');
-    this.dialogEl.classList.remove('Dialog--inactive');
-    this.setStyles({ offsetY: this.contentHeight });
+    if (this.canMinimize) {
+      this.minimize();
+    } else {
+      document.body.style.setProperty('overflow', 'auto');
+      this.dialogEl.classList.remove('Dialog--open');
+      this.dialogEl.classList.remove('Dialog--inactive');
+      this.minimized = false;
+      this.setStyles({ offsetY: this.contentHeight });
+    }
   }
 
   setStyles({ offsetY }) {
@@ -244,6 +293,7 @@ class Dialog extends HTMLElement {
       this.dragEl.addEventListener('mousedown', this.handleTouchStart);
     }
   }
+
   removeEventListeners() {
     this.overlayEl.removeEventListener('click', this.handleOverlayClick);
     this.dragEl.removeEventListener('touchstart', this.handleTouchStart);
@@ -251,13 +301,14 @@ class Dialog extends HTMLElement {
   }
 
   handleTouchStart(evt) {
-    if (this.lastTouchStartTS && evt.timeStamp - this.lastTouchStartTS < 500) {
+    if (this.canMinimize && this.lastTouchStartTS && evt.timeStamp - this.lastTouchStartTS < 500) {
       // treat as double click
       this.lastTouchStartTS = evt.timeStamp;
       if (this.minimized) return this.open();
       return this.minimize();
     }
 
+    this.lastTouch = null;
     this.lastTouchStartTS = evt.timeStamp;
     this.dialogEl.classList.add('Dialog--dragging');
     this.dragStartDialogOffset = parseInt(this.dialogEl.style.getPropertyValue('--Dialog-offset-y'), 10) || 0;
@@ -272,11 +323,15 @@ class Dialog extends HTMLElement {
   }
 
   handleTouchMove(evt) {
-    const delta = this.dragStartDialogOffset + (getPageY(evt) - this.dragStartPageY);
-    this.dialogEl.style.setProperty(
-      '--Dialog-offset-y',
-      CSS.px(clamp({ min: 0, value: delta, max: this.contentHeight - this.minContentHeight }))
-    );
+    const pageY = getPageY(evt);
+    const delta = this.dragStartDialogOffset + (pageY - this.dragStartPageY);
+    const offsetY = this.getOffsetY(delta);
+    this.lastTouch = { pageY, velocity: this.lastTouch ? pageY - this.lastTouch.pageY : 0, offsetY };
+    this.dialogEl.style.setProperty('--Dialog-offset-y', CSS.px(offsetY));
+  }
+
+  getOffsetY(value) {
+    return clamp({ min: 0, value, max: this.contentHeight - this.headerHeight });
   }
 
   handleTouchEnd() {
@@ -286,6 +341,19 @@ class Dialog extends HTMLElement {
     window.removeEventListener('mouseup', this.handleTouchEnd);
     window.removeEventListener('touchmove', this.handleTouchMove);
     window.removeEventListener('touchend', this.handleTouchEnd);
+
+    if (!this.lastTouch) return;
+    const shouldSnapToBottom = this.contentHeight - this.lastTouch.offsetY < this.snapToBottomThreshold;
+    if (this.canMinimize && shouldSnapToBottom) {
+      this.minimize();
+      return;
+    }
+    if (this.canMinimize && this.minimized) {
+      this.open();
+      return;
+    }
+    const animateOutPos = this.getOffsetY(this.lastTouch.offsetY + this.lastTouch.velocity * 4);
+    this.dialogEl.style.setProperty('--Dialog-offset-y', CSS.px(animateOutPos));
   }
 }
 
--- src/Dialog/Dialog.js
+++ src/Dialog/Dialog.js
@@ -2,6 +2,8 @@ import styles from './Dialog.css';
 
 const clamp = ({ min, value, max }) => Math.min(Math.max(value, min), max);
 const getPageY = (evt) => (evt.type.startsWith('touch') ? evt.touches[0].pageY : evt.pageY);
+const isElement = ({ nodeType }) => nodeType === 1;
+const toBool = (value) => (value != null ? true : false);
 
 const template = () => `
 <style>${styles}</style>
@@ -9,6 +11,7 @@ const template = () => `
   <div class="Dialog__overlay" role="button"></div>
   <div class="Dialog__content">
     <div class="Dialog__drag-handle"><span class="Dialog__drag-icon"></span></div>
+    <slot name="header"></slot>
     <slot></slot>
   </div>
 </div>
@@ -16,7 +19,7 @@ const template = () => `
 
 class Dialog extends HTMLElement {
   static get observedAttributes() {
-    return ['open', 'min-content-height', 'no-resize'];
+    return ['open', 'no-resize', 'minimize', 'snap-to-top'];
   }
 
   constructor() {
@@ -25,41 +28,80 @@ class Dialog extends HTMLElement {
     const shadow = this.attachShadow({ mode: 'open' });
     shadow.innerHTML = template();
 
+    /**
+     * elements
+     */
+    this.dialogEl = this.shadowRoot.querySelector('.Dialog');
+    this.overlayEl = this.shadowRoot.querySelector('.Dialog__overlay');
+    this.contentWrapperEl = this.shadowRoot.querySelector('.Dialog__content');
+    this.contentEl = this.shadowRoot
+      .querySelector('slot:not([name])')
+      .assignedNodes()
+      .find(isElement);
+    this.headerEl = this.shadowRoot
+      .querySelector('slot[name="header"]')
+      .assignedNodes()
+      .find(isElement);
+    this.dragEl = this.shadowRoot.querySelector('.Dialog__drag-handle');
+
+    /**
+     * props
+     */
     this.isOpen = false;
     this.connected = false;
     this.noResize = false;
+    this.canMinimize = false;
     this.minimized = false;
-    this.contentHeight = 0;
-    this.minContentHeight = 34; // height of header
+    this.snapToTop = false;
+    this.snapToBottomThreshold = 0;
+    this.contentHeight = this.contentWrapperEl.offsetHeight;
+    this.headerHeight = this.headerEl ? this.headerEl.offsetHeight : 0;
     this.isTouchDevice = navigator.maxTouchPoints > 0;
 
+    /**
+     * Bound methods
+     */
     this.close = this.close.bind(this);
     this.handleTouchStart = this.handleTouchStart.bind(this);
     this.handleTouchMove = this.handleTouchMove.bind(this);
     this.handleTouchEnd = this.handleTouchEnd.bind(this);
     this.handleOverlayClick = this.handleOverlayClick.bind(this);
-
-    this.dialogEl = this.shadowRoot.querySelector('.Dialog');
-    this.overlayEl = this.shadowRoot.querySelector('.Dialog__overlay');
-    this.contentEl = this.shadowRoot.querySelector('.Dialog__content');
-    this.dragEl = this.shadowRoot.querySelector('.Dialog__drag-handle');
   }
 
   attributeChangedCallback(name, oldVal, newValue) {
     switch (name) {
       case 'open': {
-        this.isOpen = newValue != null ? true : false;
-        if (this.isOpen) return this.open();
-        return this.close();
+        this.isOpen = toBool(newValue);
+        if (this.isOpen) {
+          this.open();
+        } else if (this.canMinimize) {
+          this.setAttribute('open', '');
+          this.minimize();
+        } else {
+          this.close();
+        }
+        break;
       }
-      case 'min-content-height': {
-        this.minContentHeight = Number(newValue);
-        return;
+      case 'snap-to-top': {
+        this.snapToTop = toBool(newValue);
+        if (this.snapToTop) {
+          const height = window.innerHeight - this.headerHeight - this.dragEl.offsetHeight;
+          this.contentEl.style.setProperty('height', height + 'px');
+          this.contentHeight = this.contentWrapperEl.offsetHeight;
+        } else {
+          this.contentEl.style.removeProperty('height');
+        }
+        break;
       }
       case 'no-resize': {
-        this.noResize = newValue != null ? true : false;
+        this.noResize = toBool(newValue);
         this.dialogEl.classList[this.noResize ? 'add' : 'remove']('Dialog--no-resize');
-        return;
+        break;
+      }
+      case 'minimize': {
+        this.canMinimize = toBool(newValue);
+        this.snapToBottomThreshold = window.innerHeight * 0.3;
+        break;
       }
       default: {
         console.log('unhandled attr change:', name);
@@ -69,23 +111,26 @@ class Dialog extends HTMLElement {
 
   connectedCallback() {
     this.connected = true;
-    this.contentHeight = this.contentEl.offsetHeight;
     this.registerEventListeners();
     this.setStyles({ offsetY: this.contentHeight });
+    if (this.canMinimize && !this.isOpen) {
+      this.setAttribute('open', '');
+      this.minimize();
+    }
   }
 
   disconnectedCallback() {
     this.removeEventListeners();
+    document.body.style.removeProperty('overflow');
   }
 
   handleOverlayClick(evt) {
     if (evt.target !== this.overlayEl) return;
-    this.isOpen = false;
-    this.removeAttribute('open');
-    this.dialogEl.classList.remove('Dialog--open');
+    this.close();
   }
 
   open(retries = 5) {
+    // because attribute change fire before connected-hook
     if (!this.connected && retries) return requestAnimationFrame(() => this.open(--retries));
     this.minimized = false;
     document.body.style.setProperty('overflow', 'hidden');
@@ -98,15 +143,19 @@ class Dialog extends HTMLElement {
     this.minimized = true;
     document.body.style.setProperty('overflow', 'auto');
     this.dialogEl.classList.add('Dialog--inactive');
-    this.setStyles({ offsetY: this.contentHeight - this.minContentHeight });
+    this.setStyles({ offsetY: this.contentHeight - this.headerHeight });
   }
 
   close() {
-    this.minimized = false;
-    document.body.style.setProperty('overflow', 'auto');
-    this.dialogEl.classList.remove('Dialog--open');
-    this.dialogEl.classList.remove('Dialog--inactive');
-    this.setStyles({ offsetY: this.contentHeight });
+    if (this.canMinimize) {
+      this.minimize();
+    } else {
+      document.body.style.setProperty('overflow', 'auto');
+      this.dialogEl.classList.remove('Dialog--open');
+      this.dialogEl.classList.remove('Dialog--inactive');
+      this.minimized = false;
+      this.setStyles({ offsetY: this.contentHeight });
+    }
   }
 
   setStyles({ offsetY }) {
@@ -123,6 +172,7 @@ class Dialog extends HTMLElement {
       this.dragEl.addEventListener('mousedown', this.handleTouchStart);
     }
   }
+
   removeEventListeners() {
     this.overlayEl.removeEventListener('click', this.handleOverlayClick);
     this.dragEl.removeEventListener('touchstart', this.handleTouchStart);
@@ -130,13 +180,14 @@ class Dialog extends HTMLElement {
   }
 
   handleTouchStart(evt) {
-    if (this.lastTouchStartTS && evt.timeStamp - this.lastTouchStartTS < 500) {
+    if (this.canMinimize && this.lastTouchStartTS && evt.timeStamp - this.lastTouchStartTS < 500) {
       // treat as double click
       this.lastTouchStartTS = evt.timeStamp;
       if (this.minimized) return this.open();
       return this.minimize();
     }
 
+    this.lastTouch = null;
     this.lastTouchStartTS = evt.timeStamp;
     this.dialogEl.classList.add('Dialog--dragging');
     this.dragStartDialogOffset = parseInt(this.dialogEl.style.getPropertyValue('--Dialog-offset-y'), 10) || 0;
@@ -151,11 +202,15 @@ class Dialog extends HTMLElement {
   }
 
   handleTouchMove(evt) {
-    const delta = this.dragStartDialogOffset + (getPageY(evt) - this.dragStartPageY);
-    this.dialogEl.style.setProperty(
-      '--Dialog-offset-y',
-      CSS.px(clamp({ min: 0, value: delta, max: this.contentHeight - this.minContentHeight }))
-    );
+    const pageY = getPageY(evt);
+    const delta = this.dragStartDialogOffset + (pageY - this.dragStartPageY);
+    const offsetY = this.getOffsetY(delta);
+    this.lastTouch = { pageY, velocity: this.lastTouch ? pageY - this.lastTouch.pageY : 0, offsetY };
+    this.dialogEl.style.setProperty('--Dialog-offset-y', CSS.px(offsetY));
+  }
+
+  getOffsetY(value) {
+    return clamp({ min: 0, value, max: this.contentHeight - this.headerHeight });
   }
 
   handleTouchEnd() {
@@ -165,6 +220,19 @@ class Dialog extends HTMLElement {
     window.removeEventListener('mouseup', this.handleTouchEnd);
     window.removeEventListener('touchmove', this.handleTouchMove);
     window.removeEventListener('touchend', this.handleTouchEnd);
+
+    if (!this.lastTouch) return;
+    const shouldSnapToBottom = this.contentHeight - this.lastTouch.offsetY < this.snapToBottomThreshold;
+    if (this.canMinimize && shouldSnapToBottom) {
+      this.minimize();
+      return;
+    }
+    if (this.canMinimize && this.minimized) {
+      this.open();
+      return;
+    }
+    const animateOutPos = this.getOffsetY(this.lastTouch.offsetY + this.lastTouch.velocity * 4);
+    this.dialogEl.style.setProperty('--Dialog-offset-y', CSS.px(animateOutPos));
   }
 }
 
